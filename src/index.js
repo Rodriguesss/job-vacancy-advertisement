bbb593e1dd9a74919fa78cbaa6493e7c75a44503
rodriguesss <viniciusrdefreitas@gmail.com>
2021-12-23 15:58:55 -0300

    start project structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+import './assets/css/reset.css'
+import './assets/css/style.css'
+
+ReactDOM.render(<App />, document.querySelector('.root'))
